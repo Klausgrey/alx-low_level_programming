fa3fc0e930761b1f3cc590ecd09405a2ce4d85bf
“Nicholas” <danielnick210@gmail.com>
2023-02-17 03:24:05 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - the function
- *
+ *!
  * Return: Always 0
  */
 int main(void)
@@ -11,4 +11,5 @@ int main(void)
 	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %i byte(s)\n", sizeof(float));
+	return (0);
 }
