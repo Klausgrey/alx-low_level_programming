604f8fe971a31787415205cc92ac000ab4dc098f
“Nicholas” <danielnick210@gmail.com>
2023-02-21 10:27:35 -0800

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isalpha - is the main function
+ *
+ * @c: is a paramter function
+ * Return: 1 if it is an alphabet else returns 0
+ */
+
+int _isalpha(int c)
+{
+	if ((c <= 97 && c >= 122) || (c <= 65 && c >= 90))
+		return (1);
+	else
+		return (0);
+}
