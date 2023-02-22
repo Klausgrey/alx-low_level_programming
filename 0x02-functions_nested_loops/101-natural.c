e2a017b7ec406b6016b2eecefbd334b46de7634a
“Nicholas” <danielnick210@gmail.com>
2023-02-22 15:04:08 -0800

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,15 +6,15 @@
  *   *
  *    * Return: Always 0.
  */
-
- int main(void)
+int main(void)
 {
-	  int a, b;
-	  for (a = 1; a < 1024; a++)
-	  {
-		  if ((a % 3) == 0 || (a % 5) == 0)
-			  b += a;
-	  }
-	  printf("%d\n", b);
-	  return (0);
+	int a, b;
+
+	for (a = 1; a < 1024; a++)
+	{
+		if ((a % 3) == 0 || (a % 5) == 0)
+			b += a;
+	}
+	printf("%d\n", b);
+	return (0);
 }
