d6aeea9785f808618857fe1508058509b3468fdc
“Nicholas” <danielnick210@gmail.com>
2023-03-03 17:58:21 -0800

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -12,7 +12,7 @@ char *string_toupper(char *str)
 
 	while (str[index] != '\0')
 	{
-		if (str[index] >= "a" && str[index] <= "z")
+		if (str[index] >= 'a' && str[index] <= 'z')
 			str[index] = str[index] - 32;
 			index++;
 	}
