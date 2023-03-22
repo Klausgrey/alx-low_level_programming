fc84685d2e7bfadf43ad2653a992ab091833fcb7
“Nicholas” <danielnick210@gmail.com>
2023-03-22 07:18:04 -0700

    all set

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,11 +1,4 @@
-#include <stdio.h>
-/**
- * main - prints the name of the file
- *
- * Return: Always 0 (Success)
- */
-int ain(void)
-{
-printf("%s\n", __FILE__);
-return (0);
-}
+#ifndef PI_H
+#define PI_H
+#define PI 3.14159265359
+#endif
--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,4 +1,11 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
-#define ABS(x) ((x) < (0) ? -(x) : (x))
-#endif
+#include <stdio.h>
+/**
+ * main - prints the name of the file
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
