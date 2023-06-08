41bbd6a89be47d7c5e27c23371378140d94fac2f
Klausgrey <danielnick210@gmail.com>
2023-06-08 08:31:43 -0700

    square & prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,7 +1,9 @@
 #include "main.h"
 int act_is_prime_number(int num, int i);
 /**
- * is_prime_number - 
+ * is_prime_number - returns the prime ofa number
+ * @n: parameter
+ * Return: 0
  */
 
 int is_prime_number(int n)
@@ -12,20 +14,25 @@ int is_prime_number(int n)
 }
 
 int act_is_prime_number(int num, int i)
+/**
+ * act_is_prime_number - funtion that calculates if the int is prime or not
+ * @num: parameter
+ * @i: iterator
+ * Return: 0 if prime & 1 if mot prime
+ */
 {
-
-    if (num <= 1) {
-        return 0;
-    }
-
-    if (i == 1) {
-        return 1;
-    }
-
-    if (num % i == 0) {
-        return 0;
-    }
-
-    return (act_is_prime_number(num, i - 1));
+	if (num <= 1)
+	{
+		return (0);
+	}
+	if (i == 1)
+	{
+		return (1);
+	}
+	if (num % i == 0)
+	{
+		return (0);
+	}
+	return (act_is_prime_number(num, i - 1));
 
 }
