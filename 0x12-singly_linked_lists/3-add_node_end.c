fdcc0c471513d88ebb7576d5a7b51b3fbafba046
“Nicholas” <danielnick210@gmail.com>
2023-07-04 15:54:08 +0100

    a littlr code modifications

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,33 +1,38 @@
-#include <stdlib.h>
-#include <string.h>
 #include "lists.h"
+#include <string.h>
 /**
-* add_node_end - adds a new node at the end of a linked list
-* @head: double pointer to the list_t list
-* @str: string to put in the new node
-*
-* Return: address of the new element, or NULL if it failed
+ * add_node_end - adds a new node to the end
+ * @head: pointer to the list
+ * @str: string to be add to the node
+ * Return: address of thr new node
 */
+
 list_t *add_node_end(list_t **head, const char *str)
 {
-list_t *new;
-list_t *temp = *head;
-unsigned int len = 0;
-while (str[len])
-len++;
-new = malloc(sizeof(list_t));
-if (!new)
-return (NULL);
-new->str = strdup(str);
-new->len = len;
-new->next = NULL;
-if (*head == NULL)
-{
-*head = new;
-return (new);
-}
-while (temp->next)
-temp = temp->next;
-temp->next = new;
-return (new);
+	list_t *new_node;
+	list_t *temp = *head;
+	unsigned int c = 0;
+
+	while (str[c])
+	c++;
+
+		new_node = (list_t *)malloc(sizeof(list_t)); /* typecasting is fun */
+		if (!new_node)
+		return (NULL);
+		new_node->str = strdup(str);
+		new_node->len = c;
+		new_node->next = NULL;
+
+		if (*head == NULL)
+		{
+			*head = new_node;
+			return (new_node);
+		}
+		while (temp->next)
+		temp = temp->next;
+		temp->next = new_node;
+
+
+	return (new_node);
+
 }
--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,18 +1,19 @@
-#include <stdlib.h>
 #include "lists.h"
+
 /**
-* free_list - frees a linked list
-* @head: list_t list to be freed
+ * free_list - fress the allocated memory space
+ * @head: pointer to..
+ * Returns nothing.
 */
+
 void free_list(list_t *head)
 {
-list_t *temp;
-while (head)
-{
-temp = head->next;
-free(head->str);
-free(head);
-head = temp;
-}
-}
+	list_t *tmp;
 
+	do {
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	} while (head != NULL);
+}
