7923207fcc925fbd0fc55874c4daa27d339ae563
Nicholas <danielnick210@gmail.com>
2023-10-30 12:50:49 +0100

    bugfix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,22 +11,21 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-	int check, len;
+	int file;
+	int i = 0, len;
 
-	if ((fd < 0) || (filename == NULL))
+	file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if ((file < 0) || (filename == NULL))
 	return (-1);
 	if (text_content == NULL)
 	{
-		close(fd);
+		close(file);
 		return (1);
 	}
-	len = 0;
-	while (text_content[len] != '\0')
-	len++;
-	check = write(fd, text_content, len);
-	close(fd);
-	if (check < 0)
+	for (len = 0; text_content[len] != '\0'; len++)
+	i = write(file, text_content, len);
+	close(file);
+	if (i < 0)
 	return (-1);
 	return (1);
 }
