3d2687935f778218c1f0f6bbdaa14d6695875dfe
Nicholas <danielnick210@gmail.com>
2023-12-11 13:08:31 +0100

    100-operation

---
+++ 0x18-dynamic_libraries/all.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+/**
+* add - adds two numbers
+* @a: paramter
+* @b: parameter
+*/
+
+int add(int a , int b)
+{
+	return (a + b);
+}
+
+/**
+* sub - subtracts two numbers
+* @a: paramter
+* @b: parameter
+*/
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* mul - multiplies two numbers
+* @a: paramter
+* @b: parameter
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* mod - gets the remnainder two numbers
+* @a: paramter
+* @b: parameter
+*/
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
